a4abcc25a7b6332d7a292538aa19aab22e0f736e
abditaresadabela <oumuamua192017@gmail.com>
2023-03-31 23:31:22 +0530

    py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urlib package
+"""
+A Python script that fetches the status of https://alx-intranet.hbtn.io/status
 """
 
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        response = request.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        data = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode("utf-8")))

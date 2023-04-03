9b709cff54bcde0290b5d8afd09605afcab5f6fe
abditaresadabela <oumuamua192017@gmail.com>
2023-04-03 11:10:42 +0530

    sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI $1 | grep "Content-Length" | cut -d " " -f2
+#!/bin/bash
+# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
